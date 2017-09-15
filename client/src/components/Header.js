34e170e4235191ad1c3ae524000d6e0ad5b2dfc2
David DeWulf <dwdewul@gmail.com>
2017-09-15 16:49:57 -0500

    Finish wiring up auth reducer Add logic to render depending on if user is logged in or not Add route to passport.js for auth callback

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li><a href="auth/google">Login</a></li>
+                )
+            default:
+                return <li><a href="api/logout">Logout</a></li>
+        }
+    }
+
     render() {
+        console.log(this.props);
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                <Link 
+                    to={this.props.auth ? '/surveys': '/'} 
+                    className="left brand-logo"
+                >
+                Emailify
+                </Link>
+                <ul id="nav-mobile" className="right">
+                    {this.renderContent()}
+                </ul>
+                </div>
+            </nav>
         )
     }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
