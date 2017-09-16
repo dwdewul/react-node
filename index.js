fcfb0edd821888f3e06156df5183d1c5b4363678
David DeWulf <dwdewul@gmail.com>
2017-09-16 08:30:36 -0500

    Add final config steps for heroku deployment

--- index.js
+++ index.js
@@ -24,5 +24,16 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === 'production'){
+  // express will serve up static assets (css and js)
+  app.use(express.static('client/build'));
+} else {
+  //express will serve up index.html if it doesn't know the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
