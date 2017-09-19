31c6c84dd507eee6a3fa03186a40dfb1ee4496b0
David DeWulf <dwdewul@gmail.com>
2017-09-19 16:40:15 -0500

    Add list of surveys created by the user Add modify Survey route to get user feedback from emails and save to DB

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -27,7 +27,7 @@ class Header extends Component {
         // console.log(this.props);
         return (
             <nav>
-                <div className="nav-wrapper indigo darken-1">
+                <div className="nav-wrapper amber darken-1">
                 <Link 
                     to={this.props.auth ? '/surveys': '/'} 
                     style={{ paddingLeft: 10 }}
--- models/Survey.js
+++ models/Survey.js
@@ -8,7 +8,7 @@ const SurveySchema = new Schema({
     subject: String,
     recipients: [RecipientSchema],
     yes: { type: Number, default: 0 },
-    no: { type: Number, default: 0},
+    no: { type: Number, default: 0 },
     _user: { type: Schema.Types.ObjectId, ref: 'User'},
     dateSent: Date,
     lastResponded: Date
